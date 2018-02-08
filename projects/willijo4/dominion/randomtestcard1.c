762fd0858fda4acee26327dcd8ab1f32afabc47d
Joshua Williams <jshwilliams26@gmail.com>
2018-02-08 02:29:20 -0800

    Starting assignment 4

---
+++ projects/willijo4/dominion/randomtestcard1.c
@@ -0,0 +1,13 @@
+//****************************************************************
+// Name: Josh Williams (willijo4)
+// Assignment: 4
+// Description: Random tester for  card
+//****************************************************************
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
