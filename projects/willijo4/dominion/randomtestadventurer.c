11be8019f70e891bb4462546ea8cd10ce2b2012c
Joshua Williams <jshwilliams26@gmail.com>
2018-02-10 23:41:29 -0800

    started testing for adventurer

--- projects/willijo4/dominion/randomtestadventurer.c
+++ projects/willijo4/dominion/randomtestadventurer.c
@@ -11,6 +11,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <time.h>
 
 /*
 Set up
@@ -23,6 +24,8 @@ Test different hand pos
 
 
 int main() {
+	srand(time(NULL));
+
 	// Initialize gameState Struct
 	struct gameState teststate;
     int handPos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
@@ -31,135 +34,51 @@ int main() {
 
 	int i, j;
 	int numplayers = 2;
-	memset(&teststate, '\0', sizeof(struct gameState));
-	printf("\n*** Initializing game with 2 players ***\n");
-	initializeGame(numplayers, cards, 2000, &teststate);
-	printf("--- Showing the initial state of each player's hand and deck\n");
-	for (i = 0; i < teststate.numPlayers; i++) {
-		printf("Current player: %d\n", i);
-		printf("Hand count for player %d: %d\n", i, teststate.handCount[i]);
-		printf("Deck count for player %d: %d\n", i, teststate.deckCount[i]);
-		int treasurehandcount = 0;
-		for (j = 0; j < teststate.handCount[i]; j++) {
-			if (teststate.hand[i][j] == copper || teststate.hand[i][j] == silver || teststate.hand[i][j] == gold) {
-				treasurehandcount++;
-			}
-		}
-		printf("Treasure cards in hand for player %d: %d\n", i, treasurehandcount);
-		printf("Card count for player %d for copper: %d\n", i, fullDeckCount(i, copper, &teststate));
-		printf("Card count for player %d for silver: %d\n", i, fullDeckCount(i, silver, &teststate));
-		printf("Card count for player %d for gold: %d\n", i, fullDeckCount(i, gold, &teststate));
-		printf("Card count for player %d for estate: %d\n", i, fullDeckCount(i, estate, &teststate));
-	}
-	// Setting all cards for both players to estate before starting
-	printf("--- Setting all cards in each player's hand and deck to estate\n");
-	for (i = 0; i < teststate.numPlayers; i++) {
-		for (j = 0; j < teststate.handCount[i]; j++) {
-			teststate.hand[i][j] = estate;
-		}
-		for (j = 0; j < teststate.deckCount[i]; j++) {
-			teststate.deck[i][j] = estate;
-		}
-	}
 
-	// drawntreasure will always == 0, since the function will always be passes
-	// a value of 0 for this function
-	// For player 1 (0), modify the cards, such that there are 0, 1, and 2
-	// treasure cards in their deck, and display cards in deck for each scenario
-	// The treasure cards should go to the hand when there are 2
-	
-	// 0 treasure cards
-	printf("--- Results for 0 treasure cards in each player's deck\n");
-	cardEffect(adventurer, choice1, choice2, choice3, &teststate, handPos, &bonus);
-	for (i = 0; i < teststate.numPlayers; i++) {
-		printf("Current player: %d\n", i);
-		printf("Hand count for player %d: %d\n", i, teststate.handCount[i]);
-		printf("Deck count for player %d: %d\n", i, teststate.deckCount[i]);
-		int treasurehandcount = 0;
-		for (j = 0; j < teststate.handCount[i]; j++) {
-			if (teststate.hand[i][j] == copper || teststate.hand[i][j] == silver || teststate.hand[i][j] == gold) {
-				treasurehandcount++;
+	int r;
+	for (r = 0; r < 200; r++) {
+		memset(&teststate, '\0', sizeof(struct gameState));
+		//printf("\n*** Initializing game with 2 players ***\n");
+		initializeGame(numplayers, cards, 2000, &teststate);
+		printf("--- Showing the initial state of each player's hand and deck\n");
+		for (i = 0; i < teststate.numPlayers; i++) {
+			//printf("Current player: %d\n", i);
+			//printf("Hand count for player %d: %d\n", i, teststate.handCount[i]);
+			//printf("Deck count for player %d: %d\n", i, teststate.deckCount[i]);
+			int treasurehandcount = 0;
+			for (j = 0; j < teststate.handCount[i]; j++) {
+				if (teststate.hand[i][j] == copper || teststate.hand[i][j] == silver || teststate.hand[i][j] == gold) {
+					treasurehandcount++;
+				}
+			}
+			printf("Treasure cards in hand for player %d: %d\n", i, treasurehandcount);
+		}
+		// Setting all cards for both players to estate before starting
+		printf("--- Setting all cards in each player's hand to random cards\n");
+		for (i = 0; i < teststate.numPlayers; i++) {
+			for (j = 0; j < teststate.handCount[i]; j++) {
+				teststate.hand[i][j] = (rand() % (26 + 1 - 0)) + 0;
+			}
+			for (j = 0; j < teststate.deckCount[i]; j++) {
+				teststate.deck[i][j] = (rand() % (26 + 1 - 0)) + 0;
 			}
 		}
-		printf("Treasure cards in hand for player %d == 0: %s\n", i, treasurehandcount == 0 ? "True" : "False");
-		printf("Card count for player %d for copper: %d\n", i, fullDeckCount(i, copper, &teststate));
-		printf("Card count for player %d for silver: %d\n", i, fullDeckCount(i, silver, &teststate));
-		printf("Card count for player %d for gold: %d\n", i, fullDeckCount(i, gold, &teststate));
-		printf("Card count for player %d for estate: %d\n", i, fullDeckCount(i, estate, &teststate));
-	}
-	
-		
-	// 1 treasure card
-	memset(&teststate, '\0', sizeof(struct gameState));
-	printf("\n*** Reinitializing game with 2 players ***\n");
-	initializeGame(numplayers, cards, 2000, &teststate);
-	// Setting all cards for both players to estate before starting
-	printf("--- Setting all cards in each player's hand and deck to estate and 1 copper card\n");
-	for (i = 0; i < teststate.numPlayers; i++) {
-		for (j = 0; j < teststate.handCount[i]; j++) {
-			teststate.hand[i][j] = estate;
-		}
-		for (j = 0; j < teststate.deckCount[i]-1; j++) {
-			teststate.deck[i][j] = estate;
-		}
-		teststate.deck[i][teststate.deckCount[i]-2] = copper;
-	}
-	printf("--- Results for 1 treasure card in each player's deck\n");
-	cardEffect(adventurer, choice1, choice2, choice3, &teststate, handPos, &bonus);
-	for (i = 0; i < teststate.numPlayers; i++) {
-		printf("Current player: %d\n", i);
-		printf("Hand count for player %d: %d\n", i, teststate.handCount[i]);
-		printf("Deck count for player %d: %d\n", i, teststate.deckCount[i]);
-		int treasurehandcount = 0;
-		for (j = 0; j < teststate.handCount[i]; j++) {
-			if (teststate.hand[i][j] == copper || teststate.hand[i][j] == silver || teststate.hand[i][j] == gold) {
-				treasurehandcount++;
+
+		// Random number of treasure cards
+		cardEffect(adventurer, choice1, choice2, choice3, &teststate, handPos, &bonus);
+		for (i = 0; i < teststate.numPlayers; i++) {
+			//printf("Current player: %d\n", i);
+			//printf("Hand count for player %d: %d\n", i, teststate.handCount[i]);
+			//printf("Deck count for player %d: %d\n", i, teststate.deckCount[i]);
+			int treasurehandcount = 0;
+			for (j = 0; j < teststate.handCount[i]; j++) {
+				if (teststate.hand[i][j] == copper || teststate.hand[i][j] == silver || teststate.hand[i][j] == gold) {
+					treasurehandcount++;
+				}
 			}
+			printf("Treasure cards in hand for player %d: %d\n", i, treasurehandcount);
 		}
-		printf("Treasure cards in hand for player %d == 1: %s\n", i, treasurehandcount == 1 ? "True" : "False");
-		printf("Card count for player %d for copper: %d\n", i, fullDeckCount(i, copper, &teststate));
-		printf("Card count for player %d for silver: %d\n", i, fullDeckCount(i, silver, &teststate));
-		printf("Card count for player %d for gold: %d\n", i, fullDeckCount(i, gold, &teststate));
-		printf("Card count for player %d for estate: %d\n", i, fullDeckCount(i, estate, &teststate));
 	}
-	
-	
-	
-	// 2 treasure cards
-	memset(&teststate, '\0', sizeof(struct gameState));
-	printf("\n*** Reinitializing game with 2 players ***\n");
-	initializeGame(numplayers, cards, 2000, &teststate);
-	// Setting all cards for both players to estate before starting
-	printf("--- Setting all cards in each player's hand and deck to estate and 2 copper cards\n");
-	for (i = 0; i < teststate.numPlayers; i++) {
-		for (j = 0; j < teststate.handCount[i]; j++) {
-			teststate.hand[i][j] = estate;
-		}
-		for (j = 0; j < teststate.deckCount[i]-2; j++) {
-			teststate.deck[i][j] = estate;
-		}
-		teststate.deck[i][teststate.deckCount[i]-3] = copper;
-		teststate.deck[i][teststate.deckCount[i]-2] = copper;
-	}
-	printf("--- Results for 2 treasure cards in each player's deck\n");
-	cardEffect(adventurer, choice1, choice2, choice3, &teststate, handPos, &bonus);
-	for (i = 0; i < teststate.numPlayers; i++) {
-		printf("Current player: %d\n", i);
-		printf("Hand count for player %d: %d\n", i, teststate.handCount[i]);
-		printf("Deck count for player %d: %d\n", i, teststate.deckCount[i]);
-		int treasurehandcount = 0;
-		for (j = 0; j < teststate.handCount[i]; j++) {
-			if (teststate.hand[i][j] == copper || teststate.hand[i][j] == silver || teststate.hand[i][j] == gold) {
-				treasurehandcount++;
-			}
-		}
-		printf("Treasure cards in hand for player %d == 2: %s\n", i, treasurehandcount == 2 ? "True" : "False");
-		printf("Card count for player %d for copper: %d\n", i, fullDeckCount(i, copper, &teststate));
-		printf("Card count for player %d for silver: %d\n", i, fullDeckCount(i, silver, &teststate));
-		printf("Card count for player %d for gold: %d\n", i, fullDeckCount(i, gold, &teststate));
-		printf("Card count for player %d for estate: %d\n", i, fullDeckCount(i, estate, &teststate));
-	}
-			
 		
 	return 0;
 }
