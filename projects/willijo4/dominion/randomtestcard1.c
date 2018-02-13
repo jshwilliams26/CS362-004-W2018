cd023868e1b4fbc5f65d368da828dbcf9c86591c
Joshua Williams <jshwilliams26@gmail.com>
2018-02-12 17:38:25 -0800

    started random testing for smithy_func

--- projects/willijo4/dominion/randomtestcard1.c
+++ projects/willijo4/dominion/randomtestcard1.c
@@ -1,13 +1,47 @@
 //****************************************************************
 // Name: Josh Williams (willijo4)
 // Assignment: 4
-// Description: Random tester for  card
+// Description: Random tester for smithy card
 //****************************************************************
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
 #include <stdio.h>
+#include <time.h>
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
 #include <assert.h>
+
+int main() {
+	srand(time(NULL));
+
+	// Initialize gameState Struct
+	struct gameState teststate;
+	int handPos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
+
+	int numplayers = 2;
+	int cards[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
+
+	printf("\n*** Running smithy_func for 2 players ***\n");
+
+	// Test with game state where none of the card are in the deck
+	numplayers = rand() % 2 + 2;
+	handPos = 0;//rand() % 4 + 4;	
+	memset(&teststate, '\0', sizeof(struct gameState));
+	initializeGame(numplayers, cards, 2000, &teststate);
+
+	// There should be 3 more cards after the smithy card is drawn than before
+	printf("Player %d's number of cards in hand == 5 before smithy_func: %s\n", whoseTurn(&teststate), numHandCards(&teststate) == 5 ? "True" : "False");
+	cardEffect(smithy, choice1, choice2, choice3, &teststate, handPos, &bonus);
+	printf("Player %d's number of cards in hand == 7 after smithy_func: %s\n", whoseTurn(&teststate), numHandCards(&teststate) == 7 ? "True" : "False");
+	endTurn(&teststate);
+
+	// There should be 3 more cards after the smithy card is drawn than before
+	printf("Player %d's number of cards in hand == 5 before smithy_func: %s\n", whoseTurn(&teststate), numHandCards(&teststate) == 5 ? "True" : "False");
+	cardEffect(smithy, choice1, choice2, choice3, &teststate, handPos, &bonus);
+	printf("Player %d's number of cards in hand == 7 after smithy_func: %s\n", whoseTurn(&teststate), numHandCards(&teststate) == 7 ? "True" : "False");
+	endTurn(&teststate);
+	
+	return 0;
+}
